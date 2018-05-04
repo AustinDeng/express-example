4fb46a18a61aefdfbb2585a3b0d430a30e7297de
AustinDeng <13925510108@163.com>
2018-05-04 17:42:18 +0800

    前后跑通

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,44 @@
 var express = require('express')
 var router = express.Router()
 
+var dataForm = {
+  title: '后台表单提交页面',
+  movie: {
+    _id: '',
+    title: '',
+    director: '',
+    country: '',
+    year: '',
+    poster: '',
+    language: '',
+    flash: '',
+    summary:''
+  }
+}
+
+var dataAdmin = {
+  title: '后台管理页面',
+  movies: [
+    {
+    title: '这是名字',
+    _id: 1,
+    director: '导演',
+    country: '国家',
+    year: '2018',
+    poster: '',
+    language: '',
+    flash: '',
+    summary:'这是简介'
+  }
+]
+}
+
 router.get('/movie', function(req, res, next){
-  res.render('admin', {
-    title: '后台管理页'
-  })
+  res.render('admin', dataAdmin)
 })
 
 router.get('/form', function(req, res, next){
-  res.render('form',{
-    title: '后台上传表单'
-  })
+  res.render('form',dataForm)
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,67 @@
 var express = require('express')
 var router = express.Router()
 
+var dataIndex = {
+  title: '电影首页',
+  movies: [
+    {
+      title: '巴霍巴利王2：终结',
+      _id: 1,
+      poster: 'http://p0.meituan.net/movie/3e42788f6f6283f430e74ac2381dd6ad151806.jpg@464w_644h_1e_1c'
+    },
+    {
+      title: '巴霍巴利王2：终结',
+      _id: 2,
+      poster: 'http://p0.meituan.net/movie/3e42788f6f6283f430e74ac2381dd6ad151806.jpg@464w_644h_1e_1c'
+    },
+    {
+      title: '巴霍巴利王2：终结',
+      _id: 3,
+      poster: 'http://p0.meituan.net/movie/3e42788f6f6283f430e74ac2381dd6ad151806.jpg@464w_644h_1e_1c'
+    },
+    {
+      title: '巴霍巴利王2：终结',
+      _id: 4,
+      poster: 'http://p0.meituan.net/movie/3e42788f6f6283f430e74ac2381dd6ad151806.jpg@464w_644h_1e_1c'
+    },
+    {
+      title: '巴霍巴利王2：终结',
+      _id: 5,
+      poster: 'http://p0.meituan.net/movie/3e42788f6f6283f430e74ac2381dd6ad151806.jpg@464w_644h_1e_1c'
+    },
+    {
+      title: '巴霍巴利王2：终结',
+      _id: 6,
+      poster: 'http://p0.meituan.net/movie/3e42788f6f6283f430e74ac2381dd6ad151806.jpg@464w_644h_1e_1c'
+    },
+    {
+      title: '巴霍巴利王2：终结',
+      _id: 7,
+      poster: 'http://p0.meituan.net/movie/3e42788f6f6283f430e74ac2381dd6ad151806.jpg@464w_644h_1e_1c'
+    },
+  ]
+}
+
+var dataDetail = {
+  title: '电影详情页',
+  movie: {
+    director: 'S·S·拉贾穆里',
+    title: '巴霍巴利王2：终结',
+    country: '印度',
+    language: 'English',
+    year: '2018',
+    flash:'http://player.youku.com/embed/XODY3NDMzNjY4',
+    summary: "同蛮族的大战以摩喜施末底的胜利而告终，根据战场上的表现，希瓦伽米太后（拉姆亚·克里希南饰）选择了养子阿玛兰德拉·巴霍巴利（帕拉巴斯饰）作为王国的王储。在加冕典礼之前太后交给巴霍巴利的最后一项任务是游历王国，体察臣民的疾苦，理解'臣民的审判'。同时，太后还竭尽全力满足自己的亲子巴拉拉德夫德斯（纳拉·达古巴提饰）的各种需求，希望能够克制他的贪婪，平衡兄弟的关系。巴霍巴利经过一个叫昆达拉的诸侯小国时，偶遇美丽绝伦、剑术高超的提婆犀那公主（安努舒卡·谢蒂饰），立刻惊为天人，无以自拔。"
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-     title: '电影首页' 
-    })
+  res.render('index', dataIndex)
 })
 
 router.get('/movie/:id', function(req, res, next){
-  res.render('detail',{
-    title: '电影详情页'
-  })
+  res.render('detail', dataDetail)
 })
 
 module.exports = router
